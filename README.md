fd13fc888cc921e12a5da7b721d085f47f020187
Matheus Ribeiro <mtr.matheus.ribeiroc@gmail.com>
2023-04-22 17:43:40 -0300

    Segundo commit
    Atualização de arquivos

--- README.md
+++
@@ -1,2 +0,0 @@
-# ProjetoInterdiciplinar3S3P
- Projeto interdiciplinar do 3° semestre do curso de Análise e Desenvolvimento de Sistemas - 3°P
---
+++ src/main/java/com/mycompany/projeto/Projeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto;
+
+/**
+ *
+ * @author mathe
+ */
+public class Projeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
